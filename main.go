4354333aac4fe044599fe5e603e06134a1e73456
JayaKrishnavamsi <jayakrishnavamsi4@gmail.com>
2021-03-10 11:20:39 +0530

    deletion

--- main.go
+++ main.go
@@ -4,14 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
-	fmt.Println("First Change local")
-	fmt.Println("First Change online")
-	fmt.Println("second change")
-	fmt.Println("third")
-	fmt.Println("4TH")
-	fmt.Println("5TH")
-	fmt.Println("tired of 6th")
-	fmt.Println("signing")
-	fmt.Println("tt")
-	fmt.Println("gvgafsgj")
+
 }
